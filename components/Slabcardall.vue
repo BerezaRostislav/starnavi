34899c03389410f1bc04f7356fb2802990360994
BerezaRostislav <bereza.rostislav93@gmail.com>
2018-07-25 08:29:12 +0300

    slab design

--- components/Slabcardall.vue
+++ components/Slabcardall.vue
@@ -1,23 +1,21 @@
 <template>
-    <v-container fluid>
-        <v-layout row wrap>
-            <v-flex xs6 offset-xs1>
+    <v-container grid-list-md text-xs-center>
+        <v-layout align-start justify-center fill-height row wrap>
+            <v-flex xs10>
                 <v-card class="slabcard" >
-
-                <div class="slabfrontimage" :style="{backgroundImage: 'url('+ slabfrontimage + ')'}"></div>
-                <span>{{ full_address }}</span>
-                <span>{{ description }} </span>
-                <span>{{ public_date }} </span>
-
-                <div style="text-align: center;">    
-                <span style="margin-left: 10px;">Кол-во комнат - {{ rooms }}  </span>
-                <v-btn :to="'/' + id" style="margin-right: 10px;color:red;"> Купить за - {{ price }}$</v-btn>
-                </div>
-                    <div>
+                    <v-flex xs6>
+                        <div class="slabfrontimage" :style="{backgroundImage: 'url('+ slabfrontimage + ')'}"></div>
                         <no-ssr>
-                             <star-rating v-model="rating" :read-only="true" :show-rating="false" :glow="16" :rounded-corners="true" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"></star-rating>
+                        <star-rating v-model="rating" :read-only="true" :show-rating="false" :glow="16" :rounded-corners="true" :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]"></star-rating>
                         </no-ssr>
-                    </div>
+                    </v-flex>
+                    <v-flex xs9>
+                        <span>{{ full_address }}</span>
+                        <span>{{ description }} </span>
+                        <span>{{ public_date }} </span>
+                        <span style="margin-left: 10px;">Кол-во комнат - {{ rooms }}  </span>
+                        <v-btn :to="'/' + id" style="margin-right: 10px;color:red;"> Купить за - {{ price }}$</v-btn>
+                    </v-flex>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -45,8 +43,8 @@ a {
     flex-direction: column;
     justify-content: space-between;
     box-sizing: border-box;
-    height: 500px;
-    width: 650px;
+    /* height: 500px; */
+    width: 90%;
     padding-top: 0px;
     padding: 8px;
     border: 1px solid #ccc;
